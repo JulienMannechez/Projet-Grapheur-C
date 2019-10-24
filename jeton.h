7092a91cbde4aeb424570d8983cb3ebe0a6de1a4
Julien Mannechez <39213214+JulienMannechez@users.noreply.github.com>
2019-10-24 11:25:01 +0200

    Add files via upload
    Update du jeton.h
    Ajout de la structure couple pour la création d'un arbre en partie Evaluateur.
    Ajout de la structure typerreur en enum pour gérer les erreurs en partie Analyse Lexcical.

---
+++ jeton.h
@@ -0,0 +1,43 @@
+/*Jeton .h donné dans le cours */
+typedef enum{
+    REEL,OPERATEUR,FONCTION,ERREUR,FIN,PAR_OUV,PAR_FERM,VARIABLE,BAR_OUV,BAR_FERM,ABSOLU
+}typelexem;
+
+typedef enum{
+    PLUS,MOINS,FOIS,DIV,PUIS
+}typeoperateur;
+
+typedef enum{
+    ABS,SIN,SQRT,LOG,COS,TAN,EXP,ENTIER,VAL_NEG,SINC
+}typefonction;
+
+/* Ajout la strucutre typeerreur de type ENUM */
+typedef enum {
+    RAS, FONC_INCONNUE, PARA_ERROR, OPERATEUR_ERROR, VARIABLE_ERROR,
+}typeerreur;
+
+typedef union{
+    float reel;
+    typefonction fonction;
+    typeoperateur operateur;
+    typeerreur erreur;
+}typevaleur;
+
+typedef struct{
+    typelexem lexem;
+    typevaleur valeur;
+}typejeton;
+
+typedef struct Node{
+    typejeton jeton;
+    struct Node *pjeton_preced;
+    struct Node *pjeton_suiv;  
+} Node;
+
+typedef Node *Arbre;
+
+/* Rajout de ligne de Julien T pour stocker les couples x et y=f(x) */
+typedef struct {
+    float x;
+    float y;
+} Couple;
